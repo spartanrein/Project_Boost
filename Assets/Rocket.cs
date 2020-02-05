089016f4c223528e74f30df0b0931b497061375a
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2020-02-05 15:48:10 +0800

    feat() add rocket boost with spacebar press

---
+++ Assets/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    new Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        bool keyAPressed = Input.GetKey(KeyCode.A);
+        bool keyDPressed = Input.GetKey(KeyCode.D);
+        bool keySpacePressed = Input.GetKey(KeyCode.Space);
+
+        if (keySpacePressed)
+        {
+            print("Space pressed");
+            rigidbody.AddRelativeForce(Vector3.up);
+
+        }
+        if (keyAPressed)
+        {
+            print("A is pressed");
+        }
+        else if (keyDPressed)
+        {
+            print("D is pressed");
+        }
+    }
+}
