7c21c243b3aab4c69ed826ba8fed3c4c1bbd6928
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2020-02-05 16:51:25 +0800

    feat()add rocket sound,  update prefabs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     new Rigidbody rigidbody;
+    AudioSource rocketSound;
+    bool playRocketSound;
 
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
-		
+        rocketSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -22,21 +24,30 @@ public class Rocket : MonoBehaviour {
     {
         bool keyAPressed = Input.GetKey(KeyCode.A);
         bool keyDPressed = Input.GetKey(KeyCode.D);
-        bool keySpacePressed = Input.GetKey(KeyCode.Space);
+        bool keySpacePressed = Input.GetKey(KeyCode.L);
 
         if (keySpacePressed)
         {
             print("Space pressed");
             rigidbody.AddRelativeForce(Vector3.up);
-
+            if (!rocketSound.isPlaying)
+            {
+                rocketSound.Play();
+            }
+        }
+        else
+        {
+            rocketSound.Stop();
         }
         if (keyAPressed)
         {
             print("A is pressed");
+            transform.Rotate(Vector3.forward);
         }
         else if (keyDPressed)
         {
             print("D is pressed");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
